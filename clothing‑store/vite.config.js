ae55462858acb84ae0d9b10a37d7bc1bbcad38ee
olatunjioye <oyeolatunji@gmail.com>
2025-04-21 09:49:49 -0500

    update viteconfig

--- clothing‑store/vite.config.js
+++ clothing‑store/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'  // 👈 This changes the output folder from 'dist' to 'build'
+  }
 })
